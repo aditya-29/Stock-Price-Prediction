d34317db9428db67a1b593d3d512007d0ab2d4ed
Sundar Gopal <32165744+BadNietzsche@users.noreply.github.com>
2021-07-08 19:43:57 +0530

    Update Data_to_Firebase.py

--- Data_to_Firebase.py
+++ Data_to_Firebase.py
@@ -4,6 +4,7 @@ from firebase_admin import credentials, firestore
 from nsepy import get_history
 from datetime import date
 import numpy as np
+import datetime
 import pandas as pd
 cred = credentials.Certificate('./stock-prediction-804e2-firebase-adminsdk-4ny8o-89c56b89b9.json')
 firebase_admin.initialize_app(cred, 
@@ -11,7 +12,7 @@ firebase_admin.initialize_app(cred,
 'databaseURL': 'https://Stock prediction.firebaseio.com/'
 })
 db = firestore.client()
-doc_ref = db.collection(u'applications')# Import data
+doc_ref = db.collection(u'JINDALPOLY')# Import data
 
 
 start_date = date(2019, 1, 1)
@@ -19,7 +20,8 @@ end_date = date(2021,6, 10)
 symb = 'JINDALPOLY'
 #symb= input('Enter the company symbol code you want a history of')
 df= get_history (symbol=symb,start = start_date, end = end_date)
-df.rename(columns = {'Date':'Date',
+print(len(df.columns))
+df.rename(columns = { 'Date': symb+'Date', 
                       'Symbol':symb+'_Symbol',
                       'Series':symb+'_Series',
                       'Prev Close':symb+'_Prev Close',
@@ -37,4 +39,5 @@ df.rename(columns = {'Date':'Date',
                      },inplace = True)
 print(df)
 tmp = df.to_dict(orient='records')
+print(tmp)
 list(map(lambda x: doc_ref.add(x), tmp))
