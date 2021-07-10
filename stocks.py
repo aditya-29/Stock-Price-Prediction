3d1be454b2ae1426d655ca0c3968e467f07e17b3
iamuvg <63193781+iamuvg@users.noreply.github.com>
2021-07-10 19:39:49 +0530

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1,3 +1,4 @@
+from gspread.urls import DRIVE_FILES_API_V3_URL
 from nsepy import get_history
 from datetime import date
 import numpy as np
@@ -13,54 +14,29 @@ start_date = date(2019, 1, 1)
 end_date = date(2021,6, 10)
 symb1 = 'JINDALPOLY'
 symb2 = 'POLYPLEX'
+symb3 = 'Basket'
 df1 = get_history (symbol = symb1, start = start_date, end = end_date)
+df1=df1.drop(['Series','Symbol','Prev Close','Open','High','Low','Last','Close','Volume','Turnover','Trades','Deliverable Volume','%Deliverble'],axis=1)
 df1.rename(columns = {'Date':'Date',
-                      'Symbol':symb1+'_Symbol',
-                      'Series':symb1+'_Series',
-                      'Prev Close':symb1+'_Prev Close',
-                      'Open':symb1+'_Open',
-                      'High':symb1+'_High',
-                      'Low':symb1+'__Low',
-                      'Last':symb1+'_Last',
-                      'Close':symb1+'_Close',
-                      'VWAP':symb1+'_VWAP',
-                      'Volume':symb1+'_Vol',
-                      'Turnover':symb1+'_TO',
-                      'Trades':symb1+'_Trades',
-                      'Deliverable Volume':symb1+'_DelVol',
-                      '%Deliverble':symb1+'_%Del',                     
+                      'VWAP':symb1+'_VWAP',   
                      },inplace = True)
 
 df2 = get_history (symbol = symb2, start = start_date, end = end_date)
+df2=df2.drop(['Series','Symbol','Prev Close','Open','High','Low','Last','Close','Volume','Turnover','Trades','Deliverable Volume','%Deliverble'],axis=1)
 df2.rename(columns = {'Date':'Date',
-                      'Symbol':symb2+'_Symbol',
-                      'Series':symb2+'_Series',
-                      'Prev Close':symb2+'_Prev Close',
-                      'Open':symb2+'_Open',
-                      'High':symb2+'_High',
-                      'Low':symb2+'__Low',
-                      'Last':symb2+'_Last',
-                      'Close':symb2+'_Close',
-                      'VWAP':symb2+'_VWAP',
-                      'Volume':symb2+'_Vol',
-                      'Turnover':symb2+'_TO',
-                      'Trades':symb2+'_Trades',
-                      'Deliverable Volume':symb2+'_DelVol',
-                      '%Deliverble':symb2+'_%Del',                     
+                      'VWAP':symb2+'_VWAP',                   
                      },inplace = True)
 df = pd.merge(df1,df2, how='outer',on='Date')
 
 # fn for ratio column adder 
 def Ratio_column_adder(a,b):
-    df["x"]=df[a+'_VWAP']/df[b+'_VWAP']
-    return df["x"]
+    data=[]
+    df3=pd.DataFrame(data,columns=["x"])
+    df3["x"]=df[a+'_VWAP']/df[b+'_VWAP']
+    return df3["x"]
 
-df["Ratio"]=Ratio_column_adder(symb1,symb2)
-# Ratio column added
-#df["Ratio"]=df[symb1+'_VWAP']/df[symb2+'_VWAP']
+df["Ratio"] = Ratio_column_adder(symb1,symb2)
 
-def Basket_creator(a,b):
-    
 # Basket column added
 df["Basket"]=df["Ratio"]
 for i in range(0,len(df["Basket"])-1):
@@ -69,33 +45,60 @@ for i in range(0,len(df["Basket"])-1):
     else:
         df["Basket"][i]=(df["Basket"][i]*df[symb1+'_VWAP'][i])+df[symb2+'_VWAP'][i]
 
-# MA5 column 
-df["MA5"]=""
-for i in range(5,len(df["MA5"])-1):
-    df["MA5"][i-1]=sum(df["Basket"][i-5:i])/5 
 
-# MA20 column 
-df["MA20"]=""
-for j in range(20,len(df["MA20"])-1):
-    df["MA20"][j-1]=sum(df["Basket"][j-20:j])/20
+def Moving_Average(a,b):
+    df["test"]=""
+    df.rename(columns={"test":a+"_MA"+str(b)},inplace=True)
+    for i in range(b,len(df[a+"_MA"+str(b)])-1):
+        df[a+"_MA"+str(b)][i-1]=sum(df[a+"_VWAP"][i-b:i])/b
+    return None
+
+def Std_Dev(a):
+    df["test"]=""
+    df.rename(columns={"test":a+"_STDEV"},inplace=True)
+    for k in range(20,len(df[a+"_STDEV"])-1):
+        df[a+"_STDEV"][k-1]=stdev(df[a+"_VWAP"][k-20:k])
+    return None
+
+def Z_Score(a):
+    df["test"]=""
+    df.rename(columns={"test":a+"_Zscore"},inplace=True)
+    for l in range(20,len(df[a+"_Zscore"])-1):
+        df[a+"_Zscore"][l-1]=(df[a+"_MA5"][l-1]-df[a+"_MA20"][l-1])/df[a+"_STDEV"][l-1]
+    return None
+
+# Basket MA5 column 
+df["Basket_MA5"]=""
+for i in range(5,len(df["Basket_MA5"])-1):
+    df["Basket_MA5"][i-1]=sum(df["Basket"][i-5:i])/5
+
+# Basket MA20 column 
+df["Basket_MA20"]=""
+for j in range(20,len(df["Basket_MA20"])-1):
+    df["Basket_MA20"][j-1]=sum(df["Basket"][j-20:j])/20
 
 # STDEV of basket
-df["STDEV"]=""
-for k in range(20,len(df["STDEV"])-1):
-    df["STDEV"][k-1]=stdev(df["Basket"][k-20:k])
+df["Basket_STDEV"]=""
+for k in range(20,len(df["Basket_STDEV"])-1):
+    df["Basket_STDEV"][k-1]=stdev(df["Basket"][k-20:k])
 
-# Z-Score of the pair
-df["Zscore"]=""
-for l in range(20,len(df["Zscore"])-1):
-    df["Zscore"][l-1]=(df["MA5"][l-1]-df["MA20"][l-1])/df["STDEV"][l-1]
+# Z_Score of the basket
+Z_Score(symb3)
+df["Buy_initiate"]=""
 
-# print on screen to see output
-# print(df)  
+# Parameters of Symb1
+Moving_Average(symb1,5)
+Moving_Average(symb1,20)
+Std_Dev(symb1)
+Z_Score(symb1)
+df[symb1+"_Buy_initiate"]=""
 
-# To make into an excel sheet
-"""writer = pd.ExcelWriter(symb1+"_"+symb2+".xlsx")
-df.to_excel(writer)
-writer.save()"""
+# Parameters of Symb2
+Moving_Average(symb2,5)
+Moving_Average(symb2,20)
+Std_Dev(symb2)
+Z_Score(symb2)
+df[symb2+"_Buy_initiate"]=""
 
 # To create a google sheet and write the content
 gc = gspread.service_account(filename='json file key.json') 
@@ -111,5 +114,11 @@ gsheet.add_worksheet(rows=617,cols=31,title=NwWkSht)
 #df=pd.DataFrame()
 wks = gsheet.get_worksheet(1)
 set_with_dataframe(wks, df,include_index=True)
+
+
+
+
+        
+
 
 
